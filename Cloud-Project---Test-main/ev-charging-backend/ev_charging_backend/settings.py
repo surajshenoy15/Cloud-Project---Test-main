c11938682380d0da0ec9942c2672afb3bc2341d8
suraj shenoy <surajshenoyp@gmail.com>
2025-04-25 23:30:40 +0530

    second type 7

--- Cloud-Project---Test-main/ev-charging-backend/ev_charging_backend/settings.py
+++ Cloud-Project---Test-main/ev-charging-backend/ev_charging_backend/settings.py
@@ -86,8 +86,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://cloud-project-test-main-frontend.onrender.com",
 ]
 
 # Add these additional CORS settings
